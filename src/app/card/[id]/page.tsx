4a2e874ae90fab0922d19bd0a968a80dd0e1e7df
strongerDeer <stronger_deer@naver.com>
2024-04-25 22:03:08 +0900

    ✨ Feat: 카드 상세

--- src/app/card/[id]/page.tsx
+++ src/app/card/[id]/page.tsx
@@ -1,3 +1,41 @@
-export default function CardDetailPage() {
-  return <div className="flex gap-2 p-4">CardDetailPage</div>;
+import { getCard } from '@remote/card';
+
+export default async function CardDetailPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { name, corpName, promotion, tags, benefit } = await getCard(params.id);
+
+  const subTitle =
+    promotion != null ? removeHtmlTags(promotion?.title) : tags.join(',');
+  return (
+    <div className="">
+      <p>
+        {corpName} <strong>{name}</strong>
+      </p>
+      <p>{subTitle}</p>
+
+      <p>{tags}</p>
+      {benefit.length > 0 && (
+        <ul>
+          {benefit.map((item, index) => (
+            <li key={index}>- {item}</li>
+          ))}
+        </ul>
+      )}
+      {promotion != null && (
+        <div>
+          <h3 className="font-bold">유의사항</h3>
+
+          <div dangerouslySetInnerHTML={{ __html: promotion.terms }}></div>
+        </div>
+      )}
+
+      <button type="button">1분만에 신청하고 혜택받기</button>
+    </div>
+  );
+}
+function removeHtmlTags(text: string) {
+  return text.replace(/<\/?[^>]+(>|$)/g, '');
 }
--- src/remote/card.ts
+++ src/remote/card.ts
@@ -1,6 +1,8 @@
 import {
   QuerySnapshot,
   collection,
+  doc,
+  getDoc,
   getDocs,
   limit,
   query,
@@ -44,4 +46,12 @@ export async function getSearchCards(keyword: string) {
     id: doc.id,
     ...(doc.data() as Card),
   }));
+}
+export async function getCard(id: string) {
+  const snapshot = await getDoc(doc(collection(store, COLLECTIONS.CARD), id));
+
+  return {
+    id: snapshot.id,
+    ...(snapshot.data() as Card),
+  };
 }
